0f1e75e49dbe2c944e6acd27ad0ceb7d3342b46c
Adam Fiksen <adfiksen@microsoft.com>
2020-04-08 00:49:26 -0700

    create model data provider to get model data from blob storage

---
+++ Web/Data/IModelDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Data
+{
+    /// <summary>
+    /// Provides model inferred data
+    /// </summary>
+    public interface IModelDataProvider
+    {
+        /// <summary>
+        /// Gets the inference data from the model
+        /// </summary>
+        /// <returns></returns>
+        Task<object> GetModelDataAsync();
+    }
+}
---
+++ Web/Data/ModelDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace Web.Data
+{
+    public class ModelDataProvider : IModelDataProvider
+    {
+        private readonly BlobClient _blobClient;
+
+        public ModelDataProvider(BlobClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+
+        public async Task<object> GetModelDataAsync()
+        {
+            var modelData = await _blobClient.DownloadAsync();
+
+            var formatter = new BinaryFormatter();
+            modelData.Value.Content.Seek(0, SeekOrigin.Begin);
+            return formatter.Deserialize(modelData.Value.Content);
+        }
+    }
+}
