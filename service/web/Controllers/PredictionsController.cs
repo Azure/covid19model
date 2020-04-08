e6bf5df59faf9d8ea33a923605f5d6107693d902
Adam Fiksen <adfiksen@microsoft.com>
2020-04-08 01:01:22 -0700

    create a predictions endpoint

---
+++ service/web/Controllers/PredictionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Web.Data;
+
+namespace Web.Controllers
+{
+    /// <summary>
+    /// Provides API to return prediction data
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class PredictionsController : ControllerBase
+    {
+        private readonly IModelDataProvider _modelDataProvider;
+
+        public PredictionsController(IModelDataProvider modelDataProvider)
+        {
+            _modelDataProvider = modelDataProvider;
+        }
+
+        /// <summary>
+        /// Gets the prediction model
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<object>> GetModelAsync()
+        {
+            return await _modelDataProvider.GetModelDataAsync();
+        }
+    }
+}
