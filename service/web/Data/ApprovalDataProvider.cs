dc83479524582553c7275af604918077edcdd848
Adam Fiksen <adfiksen@microsoft.com>
2020-04-14 02:48:57 -0700

    provide a stub method to get approval data

---
+++ service/web/Data/ApprovalDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Threading.Tasks;
+
+namespace Web.Data
+{
+    /// <summary>
+    /// Provides model inferred data
+    /// </summary>
+    public class ApprovalDataProvider : IApprovalDataProvider
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="blobStorageClient"></param>
+        /// TODO: fix - this is not blob storage
+        public ApprovalDataProvider(BlobServiceClient blobStorageClient)
+        {
+
+        }
+
+        public Task<bool> IsDataApproved(DateTime dateRequested)
+        {
+            // TODO: verify if a given date's data has been approved
+            return Task.FromResult(false);
+        }
+    }
+}
---
+++ service/web/Data/IApprovalDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Web.Data
+{
+    public interface IApprovalDataProvider
+    {
+        Task<bool> IsDataApproved(DateTime dateRequested);
+    }
+}
